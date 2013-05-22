5c63641fb0ed16d103d143e0ab85bfa6dd7852bd
Bill Casarin <bill@casarin.ca>
2013-05-22 13:58:01 -0400

    Revert "Micro-optimized ™"
    This reverts commit 1ceea6d53e27e3fe921a6ecb6b4e78441ce4876d.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 /**
  * Map the given `arr` with callback `fn(val, i)`.
  *
@@ -7,10 +8,9 @@
  * @api public
  */
 
-// http://jsperf.com/map-with-allocated-array-vs-non-allocated
 module.exports = function(xs, fn){
+  var ret = [];
   var len = xs.length;
-  var ret = new Array(len);
   for (var i = 0; i < len; ++i) {
     ret.push(fn(xs[i], i));
   }
